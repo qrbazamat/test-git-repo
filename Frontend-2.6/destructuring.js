afe506a069dd8a95b37a0abaa94f72b900a17ba4
AzamatCoder <60395192+qrbazamat@users.noreply.github.com>
2022-07-08 18:16:15 +0500

    Topshiriq Front 2.6 uchun

---
+++ Frontend-2.6/destructuring.js
@@ -0,0 +1,76 @@
+// ma'lumotlarni destruksiya qilish va UI Card ga Chiqarish
+
+const users = [
+	{
+		firstName: 'Azamat',
+		lastName: 'Qurbanov',
+		tgAccount: 'azmatqurbanov',
+		skils: [
+			{
+				title: 'HTML',
+				price: 20
+			},
+			{
+				title: 'JS',
+				price: 30
+			},
+			{
+				title: 'React',
+				price: 50
+			}
+		]
+	},
+	{
+		firstName: 'Alisher',
+		lastName: 'Obidov',
+		tgAccount: 'alish1212',
+		skils: [
+			{
+				title: 'Java',
+				price: 40
+			},
+			{
+				title: 'JSpring',
+				price: 50
+			},
+			{
+				title: 'Oracle',
+				price: 45
+			}
+		]
+	},
+	{
+		firstName: 'Karim',
+		lastName: 'Odilov',
+		tgAccount: 'karim8989',
+		skils: [
+			{
+				title: 'Python',
+				price: 46
+			},
+			{
+				title: 'PyScript',
+				price: 38
+			}
+		]
+	},
+	{
+		firstName: 'Sirojiddin',
+		lastName: 'Yuldashov',
+		tgAccount: 'siroj_yurakcha_123',
+		skils: [
+			{
+				title: 'HTML',
+				price: 20
+			},
+			{
+				title: 'CSS',
+				price: 25
+			},
+			{
+				title: 'JS',
+				price: 28
+			}
+		]
+	}
+]
